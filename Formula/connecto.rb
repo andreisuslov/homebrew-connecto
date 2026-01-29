e1394c2c2c9f604f0d35451ad8bf842773ef6aad
andreisuslov <truvord@gmail.com>
2026-01-28 23:17:02 -0500

    Bump connecto to 0.4.0 - adds macOS firewall handling

--- Formula/connecto.rb
+++ Formula/connecto.rb
@@ -2,15 +2,15 @@ class Connecto < Formula
   desc "AirDrop-like SSH key pairing for your terminal"
   homepage "https://github.com/andreisuslov/connecto"
   license "MIT"
-  version "0.3.0"
+  version "0.4.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/andreisuslov/connecto/releases/download/v0.3.0/connecto-macos-aarch64.tar.gz"
-      sha256 "d919bbaffb60b5bb4ba7064be1b2e1028825c4ba5555cce2469909e78bb483c1"
+      url "https://github.com/andreisuslov/connecto/releases/download/v0.4.0/connecto-macos-aarch64.tar.gz"
+      sha256 "acf875f52323c67c049070adf4edc871a0d4568759695e2cb39f434e555968ea"
     else
-      url "https://github.com/andreisuslov/connecto/releases/download/v0.3.0/connecto-macos-x86_64.tar.gz"
-      sha256 "57e3328a73826283bbaa6453dd08039caea8f5b20b7a8550ddec431251590840"
+      url "https://github.com/andreisuslov/connecto/releases/download/v0.4.0/connecto-macos-x86_64.tar.gz"
+      sha256 "30412d123d883b6446e3fdfa47d520aac2c1159aa885fd3eb1493fae4c1b9c84"
     end
   end
 
