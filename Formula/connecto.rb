45707548404df0f87a37963bb5323f6b6d32bc3c
asuslov <andrei.suslov@avid.com>
2026-01-28 23:37:41 -0500

    Bump to 0.4.1 - fix firewall detection logic

--- Formula/connecto.rb
+++ Formula/connecto.rb
@@ -2,15 +2,15 @@ class Connecto < Formula
   desc "AirDrop-like SSH key pairing for your terminal"
   homepage "https://github.com/andreisuslov/connecto"
   license "MIT"
-  version "0.4.0"
+  version "0.4.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/andreisuslov/connecto/releases/download/v0.4.0/connecto-macos-aarch64.tar.gz"
-      sha256 "acf875f52323c67c049070adf4edc871a0d4568759695e2cb39f434e555968ea"
+      url "https://github.com/andreisuslov/connecto/releases/download/v0.4.1/connecto-macos-aarch64.tar.gz"
+      sha256 "c1904c34540e20dc516116c3499b659fe529713dbf3233e5fd0619e5cb37884b"
     else
-      url "https://github.com/andreisuslov/connecto/releases/download/v0.4.0/connecto-macos-x86_64.tar.gz"
-      sha256 "30412d123d883b6446e3fdfa47d520aac2c1159aa885fd3eb1493fae4c1b9c84"
+      url "https://github.com/andreisuslov/connecto/releases/download/v0.4.1/connecto-macos-x86_64.tar.gz"
+      sha256 "2fada6dc297a558c5af3281cab2d234e101ae55c478d32c9a73c16f996da84e2"
     end
   end
 
